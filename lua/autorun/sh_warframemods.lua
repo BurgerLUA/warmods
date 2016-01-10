33ea68f74a6694fa1e2f2fc1364137bbdec0e544
BurgerLUA <mattwasnothere@gmail.com>
2016-01-09 21:03:00 -0800

    Fixes

--- lua/autorun/sh_warframemods.lua
+++ lua/autorun/sh_warframemods.lua
@@ -5,8 +5,9 @@ local WarframeMods = {}
 WarframeMods["damage"] = {
 	Name = "Damage Boost",
 	Type = "DamageMul",
-	Slot = "Weapon",
-	RankValue = {10,20,30,40,50,60,70,80,90,100},
+	Logo = "hud/leaderboard_class_demo",
+	Desc = "Increases damage output by _%",
+	RankValue = {30,40,50,60,70,80,90,100,110,120},
 	BaseCapacity = 5,
 	CostPower = 1.25,
 }
@@ -14,22 +15,24 @@ WarframeMods["damage"] = {
 WarframeMods["health"] = {
 	Name = "Health Boost",
 	Type = "HealthMul",
-	Slot = "Weapon",
-	RankValue = {10,20,30,40,50,60,70,80,90,100},
+	Logo = "hud/leaderboard_class_medic",
+	Desc = "Increases damage resistance by _%",
+	RankValue = {40,45,50,55,60,65,60,65,70,75},
 	BaseCapacity = 5,
 	CostPower = 1.25,
 }
 
-WarframeMods["armor"] = {
-	Name = "Armor Boost",
-	Type = "ArmorMul",
-	Slot = "Weapon",
-	RankValue = {10,20,30,40,50,60,70,80,90,100},
+WarframeMods["speed"] = {
+	Name = "Speed Boost",
+	Type = "SpeedMul",
+	Logo = "hud/leaderboard_class_soldier_sergeant_crits",
+	Desc = "Increases speed by %_",
+	RankValue = {5,10,15,20,25,30,35,40,45,50},
 	BaseCapacity = 5,
 	CostPower = 1.25,
 }
 
-local NewPlayerMods = {"damage","health","armor"}
+local NewPlayerMods = {"damage","health","speed"}
 
 local DefaultDataString = "0 0 0 0 0 0 0 0 0 0"
 local DefaultDataTable = string.Explode(" ",DefaultDataString)
@@ -122,7 +125,12 @@ if SERVER then
 				local TotalData = {}
 			
 				for k,v in pairs(WarframeMods) do
-					TotalData[k] = WarDataGetModCountAll(ply,k)
+					local Data = WarDataGetModCountAll(ply,k)
+					
+					if Data then
+						TotalData[k] = Data
+					end
+					
 				end
 				
 				net.WriteTable(TotalData)
@@ -161,40 +169,41 @@ if SERVER then
 		local CurrentModCount = WarDataGetModCountSingle(ply,mod,rank)
 		
 		if CurrentModCount >= count*3 and rank < 10 then
-		
 			WarDataAdd(ply,mod,rank,-3*count)
 			WarDataAdd(ply,mod,rank+1,count)
 			ply:ChatPrint("Transmute successful")
-			--WarDataConsole(ply,"war_menu",nil)
-			
 		end
-	
 	
 	end
 	
 	
 	function WarDataImplode(data)
+		if not data then return nil end
 		data = string.Implode(" ",data) -- turns data into string, ingredient
 		return data
 	end
 	
 	function WarDataExplode(data)
+		if not data then return nil end
 		data = string.Explode(" ",data) -- turns data into table, ingredient
 		return data
 	end
 	
 	function WarDataWrite(dir,data)
+		if not (dir and data) then return nil end
 		--print("Writing " .. data .. " to " .. dir)
 		file.Write(dir,data)
 	end
 	
 	function WarDataRead(dir)
+		if not dir then return nil end
 		local Data = file.Read(dir,"DATA")
 		--print("Reading " .. Data .. " from " .. dir)
 		return Data
 	end
 	
-	function WarDropMod(ply,pos)	
+	function WarDropMod(ply,pos)
+		if not (ply and pos) then return end
 		local Object = ents.Create("ent_war_mod")
 		Object:SetPos( pos + Vector(0,0,10) )
 		Object:SetAngles(AngleRand())
@@ -309,6 +318,7 @@ if CLIENT then
 	local ShinyBase = {}
 	local DescBase = {}
 	local Desc = {}
+	local Image = {}
 
 	net.Receive("WarDataSendToClient", function(len)
 	
@@ -316,8 +326,8 @@ if CLIENT then
 		
 		--PrintTable(Data)
 		
-		local x = 1000
-		local y = 1000
+		local x = 800
+		local y = 600
 		
 		local Small = x*0.01
 	
@@ -381,6 +391,14 @@ if CLIENT then
 				
 				TransmuteOptions[Count]:Open()
 			end
+			
+			local ImgSize = math.floor(BaseX*0.25)
+			
+			Image[Count] = vgui.Create("DImage",Frame[Count])
+			Image[Count]:SetPos(BaseX*0.5 - ImgSize/2,BaseY*0.25 - ImgSize/2)
+			Image[Count]:SetSize(ImgSize,ImgSize)
+			Image[Count]:SetImage( OfficialData.Logo )
+			
 
 			TitleBase[Count] = vgui.Create("DPanel",Frame[Count])
 			TitleBase[Count]:SetPos(Small,BaseY*0.5 - Small)
@@ -397,12 +415,18 @@ if CLIENT then
 			DescBase[Count]:SetPos(Small,BaseY*0.6 - Small*0.5)
 			DescBase[Count]:SetSize(BaseX - Small*2,BaseY*0.35 - Small*2)
 			
+			
+			local ValueThing = tostring(OfficialData.RankValue[rank])
+			
 			Desc[Count] = vgui.Create("DLabel",DescBase[Count])
-			Desc[Count]:SetText("Faggot")
+			Desc[Count]:SetText( string.Replace(OfficialData.Desc,"_",ValueThing) )
+			Desc[Count]:SetSize(BaseX - Small*4,BaseY*0.35 - Small*4)
+			Desc[Count]:SetPos(Small,Small)
 			Desc[Count]:SetDark(true)
 			Desc[Count]:SetFont("WarFontDesc")
-			Desc[Count]:SizeToContents(true)
-			Desc[Count]:Center()
+			Desc[Count]:SetAutoStretchVertical(true)
+			Desc[Count]:SetWrap(true)
+			--Desc[Count]:Center()
 			
 			ShinyBase[Count] = vgui.Create("DPanel",Frame[Count])
 			ShinyBase[Count]:SetPos(Small, BaseY - BaseX*0.1 - Small )
@@ -410,10 +434,10 @@ if CLIENT then
 			
 			for i=1,10 do
 				if i <= rank then
-					local Shiny = vgui.Create("DPanel",ShinyBase[Count])
+					local Shiny = vgui.Create("DImage",ShinyBase[Count])
 					Shiny:SetPos( (i-1)*(BaseX-Small*2)*0.1, 0 )
 					Shiny:SetSize((BaseX-Small*2)*0.1,(BaseX-Small*2)*0.1)
-					Shiny:SetBackgroundColor(Color(255,255,0,255))
+					Shiny:SetImage("vgui/importtool_goldstar")
 				end
 			end
 			
